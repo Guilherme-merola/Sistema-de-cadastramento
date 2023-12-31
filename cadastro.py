097804987b67fad60c50933a95b18ca32f21343e
Guilherme Merola da Silva <154547316+Guilherme-merola@users.noreply.github.com>
2023-12-30 23:21:55 -0300

    Update cadastro.py

--- cadastro.py
+++ cadastro.py
@@ -1,9 +1,11 @@
 def cadastrar(nome_user, senha_user):
+    """Função responsável por adicionar um novo usuário ao txt"""
     with open('cadastros.txt', encoding='UTF-8', mode='a') as arquivo:
         arquivo.write(f"{dict(nome = nome_user, senha = senha_user)}\n")           
 
 
-def entrar(nome_user, senha_user): 
+def entrar(nome_user, senha_user):
+    """Função responsável por acessar a conta que já tenha no txt"""
     with open('cadastros.txt', encoding='UTF-8', mode='r') as arquivo:
         for user in arquivo.readlines():
             if nome_user in user and senha_user in user:
